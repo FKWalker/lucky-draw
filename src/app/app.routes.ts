51a7775ada474bcc2feadd4a475554678d7dbeab
Don <96983756+FKWalker@users.noreply.github.com>
2025-09-04 21:59:03 +0800

    add team setup module

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -18,6 +18,7 @@ import { VideosComponent } from './pages/ui-elements/videos/videos.component';
 import { SignInComponent } from './pages/auth-pages/sign-in/sign-in.component';
 import { SignUpComponent } from './pages/auth-pages/sign-up/sign-up.component';
 import { CalenderComponent } from './pages/calender/calender.component';
+import { AddTeamMemberComponent } from './pages/team/add-team-member/add-team-member.component';
 
 export const routes: Routes = [
   {
@@ -100,6 +101,11 @@ export const routes: Routes = [
       {
         path:'videos',
         component:VideosComponent,
+        title:'Angular Videos Dashboard | TailAdmin - Angular Admin Dashboard Template'
+      },
+      {
+        path:'team/add',
+        component:AddTeamMemberComponent,
         title:'Angular Videos Dashboard | TailAdmin - Angular Admin Dashboard Template'
       },
     ]
